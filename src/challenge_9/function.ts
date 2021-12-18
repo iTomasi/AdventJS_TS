b3263167a82b29e2b2e87c50ef3e80d19df549d6
Tomas Duclos <tomasmarcelo.duclos@gmail.com>
2021-12-18 02:42:17 -0300

    challenge - 9

---
+++ src/challenge_9/function.ts
@@ -0,0 +1,58 @@
+/*
+En la fábrica de Papa Noél 🎅 se acerca el día especial... y todavía tenemos un montón de cosas por contar. 😅
+
+Por suerte a Mark Zucktheelf 🧝 se le ha ocurrido crear una función que permita agrupar un array, que puede ser de valores u objetos, a través de una función o de una propiedad.
+*/
+
+const groupBy = (collection: any[], it: any) => {
+
+    const dict: any = {};
+
+    try {
+        for (let value of collection) {
+            if (typeof it === "function") {
+                const res = it(value).toString();
+
+                if (!dict[res]) {
+                    dict[res] = [value]
+                    continue
+                }
+
+                dict[res.toString()] = [...dict[res], value]
+            }
+
+            else if (typeof it === "string") {
+                if (it === "length") {
+                    const res = value.toString().length.toString();
+
+                    if (!dict[res]) {
+                        dict[res] = [value]
+                        continue
+                    }
+
+                    dict[res] = [...dict[res], value]
+                    continue
+                }
+
+                const res = value[it].toString();
+
+                if (!dict[res]) {
+                    dict[res] = [value]
+                    continue
+                }
+
+                dict[res] = [...dict[res], value]
+            }
+        }
+
+        return dict
+    }
+
+    catch (e) {
+        console.log(e);
+        console.log("groupBy() Error");
+        return {};
+    }
+}
+
+export { groupBy };
---
+++ src/challenge_9/index.ts
@@ -0,0 +1,18 @@
+/*
+En la fábrica de Papa Noél 🎅 se acerca el día especial... y todavía tenemos un montón de cosas por contar. 😅
+
+Por suerte a Mark Zucktheelf 🧝 se le ha ocurrido crear una función que permita agrupar un array, que puede ser de valores u objetos, a través de una función o de una propiedad.
+*/
+
+import { groupBy } from "./function";
+
+const exampleData: any = [
+    [{ title: 'JavaScript: The Good Parts', rating: 8 },
+    { title: 'Aprendiendo Git', rating: 10 },
+    { title: 'Clean Code', rating: 9 }],
+    "rating"
+];
+
+const response = groupBy(exampleData[0], exampleData[1]);
+
+console.log(response)
